95f0c7aa39b5c935a82d6162f49105140f8f30cf
Hunter <hunterbhb11@gmail.com>
2025-04-08 04:55:47 -0500

    added exporting for static type website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  trailingSlash: true, //Helps with S3 routing
 };
 
 export default nextConfig;
